8308f6f5c3ec32ad10c1c7ec259ae96d222d0571
Shanwaz Ghulam <ghulam.shanwaz@gmail.com>
2020-07-08 18:44:53 +0200

    Updates

---
+++ precache-manifest.b8edde4defc44fd61623b78f51a7a4e9.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29074617d4095c75b7f80dc43f8a7537",
+    "url": "/portofolio-react/index.html"
+  },
+  {
+    "revision": "7aa31f2bcf8d0f88fc71",
+    "url": "/portofolio-react/static/css/2.1f4d361e.chunk.css"
+  },
+  {
+    "revision": "e2d4b25239bf2cb00f33",
+    "url": "/portofolio-react/static/css/main.ed3d5dff.chunk.css"
+  },
+  {
+    "revision": "7aa31f2bcf8d0f88fc71",
+    "url": "/portofolio-react/static/js/2.40c293f9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portofolio-react/static/js/2.40c293f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d4b25239bf2cb00f33",
+    "url": "/portofolio-react/static/js/main.6fb2aa38.chunk.js"
+  },
+  {
+    "revision": "7580dd19cbe0fc311d80",
+    "url": "/portofolio-react/static/js/runtime-main.86eb60a1.js"
+  },
+  {
+    "revision": "21999f09aca6272cd15a16d949434923",
+    "url": "/portofolio-react/static/media/accenture-3.21999f09.svg"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/portofolio-react/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/portofolio-react/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/portofolio-react/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "e9d23d7c37d296f45ddf68a63c4cf3bb",
+    "url": "/portofolio-react/static/media/css3.e9d23d7c.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/portofolio-react/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/portofolio-react/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/portofolio-react/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/portofolio-react/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/portofolio-react/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/portofolio-react/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/portofolio-react/static/media/mssql.54d7bddc.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/portofolio-react/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/portofolio-react/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "033e67c29644c68ff113c3ca06349dc4",
+    "url": "/portofolio-react/static/media/profile.033e67c2.JPG"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/portofolio-react/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/portofolio-react/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/portofolio-react/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/portofolio-react/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/portofolio-react/static/media/sass-1.b446ca5a.svg"
+  },
+  {
+    "revision": "127850dc2a6331daf0eec1bc40487943",
+    "url": "/portofolio-react/static/media/shanwazlogo.127850dc.webp"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/portofolio-react/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/portofolio-react/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/portofolio-react/static/media/slide3.8e5b87b6.webp"
+  }
+]);
